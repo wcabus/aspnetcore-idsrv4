29fc940552cb2a27c97caef23a1c98c2e368edbb
Wesley Cabus <wesley.cabus@realdolmen.com>
2017-04-25 22:03:18 +0200

    component added

---
+++ src/Sprotify.Web/ViewComponents/LoginPanel.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Sprotify.Web.Models.User;
+using System.Threading.Tasks;
+
+namespace Sprotify.Web.ViewComponents
+{
+    public class LoginPanel : ViewComponent
+    {
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            if (!User.Identity.IsAuthenticated)
+            {
+                return View("Login");
+            }
+
+            return View(new LoginPanelModel
+            {
+                Name = User.Identity.Name
+            });
+        }
+    }
+}
